8e93fb689bc9453bdd4223d49a2f804deb501c57
aivaneezy1 <aivaneezydakii@gmail.com>
2024-08-05 11:58:34 +0200

    first commit

---
+++ app/component/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between p-4 bg-gray-500'>
+      <div className='text-white'>
+        Logo
+      </div>
+      <div className='text-white'>
+      Login
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
